8af65331daf772347b5e83ec620493a593689298
Kanchi <paulinokan97@gmail.com>
2018-12-28 13:07:37 -0600

    Pirmer Proyecto, Filtros

---
+++ Sistema/Sistema/Data/DbInitializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sistema.Models;
+
+namespace Sistema.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(SistemaContext context)
+        {
+            context.Database.EnsureCreated();
+            //busacra si existen registors en la tabla categoria
+            if (context.Categoria.Any())
+            {
+                return;
+            }
+            var categorias = new Categoria[]
+            {
+                new Categoria{Nombre="Programacion",Descripcion="Curso",Estado=true},
+                new Categoria{Nombre="Diseño",Descripcion="Curso",Estado=true}
+            };
+            foreach(Categoria c in categorias)
+            {
+                context.Categoria.Add(c);
+            }
+            context.SaveChanges();
+        }
+    }
+}
